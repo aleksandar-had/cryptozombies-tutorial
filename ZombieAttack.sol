e831313b6d0b30eaf69a0ae6ccc4a66dd83e077b
aleksandar-had <50676450+al-hadzhi@users.noreply.github.com>
2021-02-07 21:56:10 +0100

    attack win/loss results and trigger cooldown added

--- ZombieAttack.sol
+++ ZombieAttack.sol
@@ -20,8 +20,18 @@ contract ZombieAttack is ZombieHelper {
         Zombie storage myZombie = zombies[_zombieId];
         Zombie storage enemyZombie = zombies[_targetId];
         uint rand = randMod(100);
+        if (rand <= attackVictoryProbability) {
+            myZombie.winCount++;
+            myZombie.level++;
+            enemyZombie.lossCount++;
+            _feedAndMultiply(_zombieId, enemyZombie.dna, "zombie");
+        } else {
+            myZombie.lossCount++;
+            enemyZombie.winCount++;
+            _triggerCooldown(myZombie);
+        }
     }
 
-    
+
     
 }
